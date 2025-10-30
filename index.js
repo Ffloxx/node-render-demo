6c798de6a93e8b97bea15a4ff8e55ed196630c0c
Ffloxx <mohamedtariket61@gmail.com>
2025-10-30 09:21:39 +0100

    last up date

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express(); 
 const PORT = process.env.PORT || 3001; 
 app.get("/", (req, res) => { 
-res.send(" Bonjour depuis Render !"); 
+res.send(" moh lwahch howa les hommees"); 
 }); 
 app.listen(PORT, () => { 
 console.log(`Serveur lancé sur le port ${PORT}`); 
